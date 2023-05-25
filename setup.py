dbd5a38bbe4b2d62232a8a4728c654e9fb2b6f9c
Phil Wang <lucidrains@gmail.com>
2023-05-24 17:26:49 -0700

    update triton coor descent

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'mixture-of-attention',
   packages = find_packages(exclude=[]),
-  version = '0.0.18',
+  version = '0.0.19',
   license='MIT',
   description = 'Mixture of Attention',
   author = 'Phil Wang',
@@ -19,7 +19,7 @@ setup(
     'routed attention'
   ],
   install_requires=[
-    'colt5-attention>=0.10.5',
+    'colt5-attention>=0.10.6',
     'einops>=0.6.1',
     'local-attention>=1.8.6',
     'torch>=1.6',
